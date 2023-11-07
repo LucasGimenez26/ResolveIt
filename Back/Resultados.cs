483dbb19e40a8d33ca8a2cf1667a70fca01148da
Ivon <Ivon@DESKTOP-38D5R74>
2023-11-07 20:56:44 -0300

    Agregar archivos de proyecto.

---
+++ Back/Resultados.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace Back
+{
+    public class Resultados
+    {
+        public DataTable ListaRes { get; set; } = new DataTable();
+
+        public Resultados()
+        {
+            ListaRes.TableName = "Historial";
+            ListaRes.Columns.Add("Res", typeof(int));
+            leerArchivo();
+        }
+
+
+        public void leerArchivo()
+        {
+            if(System.IO.File.Exists("Resultados.xml"))
+            {
+                ListaRes.Clear();
+                ListaRes.ReadXml("Resultados.xml");
+            }
+        }
+
+        public void InsertRes(Resultado aRes)
+        {
+            ListaRes.Rows.Add(); 
+            int NuevoRenglon = ListaRes.Rows.Count - 1; 
+            ListaRes.Rows[NuevoRenglon]["Res"] = aRes.Res; 
+
+            ListaRes.WriteXml("Resultados.xml");
+        }
+
+
+
+
+
+
+
+    }
+
+}
